552519ae4e9216fb0884b89221e06a61eafbce62
chkis <chkis@DESKTOP-444NARP>
2020-01-26 12:06:47 -0800

    initial commit

---
+++ Sampleproject/src/sample/Sampleclass.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Sampleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("javaintroduction");
+
+	}
+
+}
